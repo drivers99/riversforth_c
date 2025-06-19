660640ec49d0edf7036fb6cf2406eafe5f408c8c
Don Rivers <drivers99@gmail.com>
2025-06-19 08:19:38 -0400

    add DROP SWAP OVER

--- riversforth.c
+++ riversforth.c
@@ -44,15 +44,6 @@ Cell *here = 0;
 Word *last = NULL; // head of dictionary linked list
 Word *current_word = NULL; // latest and current word really are Word pointers though
 
-void do_dup(void) {
-    Cell x = sp[0]; // wow that's cool you can use [0] on a pointer like that
-    push(x);
-}
-
-void do_plus(void) {
-    push(pop() + pop());
-}
-
 void do_dot(void) {
     printf("%ld ", pop());
 }
@@ -65,6 +56,272 @@ void do_exit(void) {
     // which is a Cell, but I guess we'd need to cast it into a Cell *
 }
 
+void do_drop(void) {
+    // drop top of stack
+    pop();
+}
+
+void do_swap(void) {
+    // swap top two elements on stack
+    Cell a = pop();
+    Cell b = pop();
+    push(a);
+    push(b);
+}
+
+void do_dup(void) {
+    // duplicate top of stack
+    Cell a = sp[0]; // wow that's cool you can use [0] on a pointer like that
+    push(a);
+}
+
+void do_over(void) {
+    // get the second element of the stack and push it on top
+    push(sp[1]);
+}
+
+/*
+
+	defcode "OVER",4,,OVER
+	mov 4(%esp),%eax	// get the second element of stack
+	push %eax		// and push it on top
+	NEXT
+
+	defcode "ROT",3,,ROT
+	pop %eax
+	pop %ebx
+	pop %ecx
+	push %ebx
+	push %eax
+	push %ecx
+	NEXT
+
+	defcode "-ROT",4,,NROT
+	pop %eax
+	pop %ebx
+	pop %ecx
+	push %eax
+	push %ecx
+	push %ebx
+	NEXT
+
+	defcode "2DROP",5,,TWODROP // drop top two elements of stack
+	pop %eax
+	pop %eax
+	NEXT
+
+	defcode "2DUP",4,,TWODUP // duplicate top two elements of stack
+	mov (%esp),%eax
+	mov 4(%esp),%ebx
+	push %ebx
+	push %eax
+	NEXT
+
+	defcode "2SWAP",5,,TWOSWAP // swap top two pairs of elements of stack
+	pop %eax
+	pop %ebx
+	pop %ecx
+	pop %edx
+	push %ebx
+	push %eax
+	push %edx
+	push %ecx
+	NEXT
+
+	defcode "?DUP",4,,QDUP	// duplicate top of stack if non-zero
+	movl (%esp),%eax
+	test %eax,%eax
+	jz 1f
+	push %eax
+1:	NEXT
+
+	defcode "1+",2,,INCR
+	incl (%esp)		// increment top of stack
+	NEXT
+
+	defcode "1-",2,,DECR
+	decl (%esp)		// decrement top of stack
+	NEXT
+
+	defcode "4+",2,,INCR4
+	addl $4,(%esp)		// add 4 to top of stack
+	NEXT
+
+	defcode "4-",2,,DECR4
+	subl $4,(%esp)		// subtract 4 from top of stack
+	NEXT
+
+	defcode "+",1,,ADD
+	pop %eax		// get top of stack
+	addl %eax,(%esp)	// and add it to next word on stack
+	NEXT
+*/
+void do_plus(void) {
+    Cell a = pop();
+    sp[0] += a;
+    //push(pop() + pop());
+}
+/*
+	defcode "-",1,,SUB
+	pop %eax		// get top of stack
+	subl %eax,(%esp)	// and subtract it from next word on stack
+	NEXT
+
+	defcode "*",1,,MUL
+	pop %eax
+	pop %ebx
+	imull %ebx,%eax
+	push %eax		// ignore overflow
+	NEXT
+
+// 
+// 	In this FORTH, only /MOD is primitive.  Later we will define the / and MOD words in
+// 	terms of the primitive /MOD.  The design of the i386 assembly instruction idiv which
+// 	leaves both quotient and remainder makes this the obvious choice.
+// 
+
+	defcode "/MOD",4,,DIVMOD
+	xor %edx,%edx
+	pop %ebx
+	pop %eax
+	idivl %ebx
+	push %edx		// push remainder
+	push %eax		// push quotient
+	NEXT
+
+// 
+// 	Lots of comparison operations like =, <, >, etc..
+
+// 	ANS FORTH says that the comparison words should return all (binary) 1's for
+// 	TRUE and all 0's for FALSE.  However this is a bit of a strange convention
+// 	so this FORTH breaks it and returns the more normal (for C programmers ...)
+// 	1 meaning TRUE and 0 meaning FALSE.
+// 
+
+	defcode "=",1,,EQU	// top two words are equal?
+	pop %eax
+	pop %ebx
+	cmp %ebx,%eax
+	sete %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode "<>",2,,NEQU	// top two words are not equal?
+	pop %eax
+	pop %ebx
+	cmp %ebx,%eax
+	setne %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode "<",1,,LT
+	pop %eax
+	pop %ebx
+	cmp %eax,%ebx
+	setl %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode ">",1,,GT
+	pop %eax
+	pop %ebx
+	cmp %eax,%ebx
+	setg %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode "<=",2,,LE
+	pop %eax
+	pop %ebx
+	cmp %eax,%ebx
+	setle %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode ">=",2,,GE
+	pop %eax
+	pop %ebx
+	cmp %eax,%ebx
+	setge %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode "0=",2,,ZEQU	// top of stack equals 0?
+	pop %eax
+	test %eax,%eax
+	setz %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode "0<>",3,,ZNEQU	// top of stack not 0?
+	pop %eax
+	test %eax,%eax
+	setnz %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode "0<",2,,ZLT	// comparisons with 0
+	pop %eax
+	test %eax,%eax
+	setl %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode "0>",2,,ZGT
+	pop %eax
+	test %eax,%eax
+	setg %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode "0<=",3,,ZLE
+	pop %eax
+	test %eax,%eax
+	setle %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode "0>=",3,,ZGE
+	pop %eax
+	test %eax,%eax
+	setge %al
+	movzbl %al,%eax
+	pushl %eax
+	NEXT
+
+	defcode "AND",3,,AND	// bitwise AND
+	pop %eax
+	andl %eax,(%esp)
+	NEXT
+
+	defcode "OR",2,,OR	// bitwise OR
+	pop %eax
+	orl %eax,(%esp)
+	NEXT
+
+	defcode "XOR",3,,XOR	// bitwise XOR
+	pop %eax
+	xorl %eax,(%esp)
+	NEXT
+
+	defcode "INVERT",6,,INVERT // this is the FORTH bitwise "NOT" function (cf. NEGATE and NOT)
+	notl (%esp)
+	NEXT
+
+*/
+
+
 void docol(void) {
     rp--; // Cell *
     *rp = (Cell)ip; // rp is Cell *.  *rp is Cell.  ip is Cell *.  Cast to Cell.
@@ -72,9 +329,13 @@ void docol(void) {
 }
 
 //                 link  name    code     params (e.g.docol code body)
+Word word_drop = { NULL, "DROP", do_drop, NULL };
+Word word_swap = { NULL, "SWAP", do_swap, NULL };
+Word word_dup  = { NULL, "DUP",  do_dup,  NULL };
+Word word_over = { NULL, "OVER", do_over, NULL };
+
 Word word_dot  = { NULL, ".",    do_dot,  NULL };
 Word word_plus = { NULL, "+",    do_plus, NULL };
-Word word_dup  = { NULL, "DUP",  do_dup,  NULL };
 Word word_exit = { NULL, "EXIT", do_exit, NULL };
 
 Word *double_body[] = { &word_dup, &word_plus, &word_exit };
@@ -167,10 +428,15 @@ int main(void)
     // interesting that the words can be added in another order
     // we probably shouldn't do that
     // going to arrange it in an order that could happen
+    add_word(&word_drop);
+    add_word(&word_swap);
     add_word(&word_dup);
+    add_word(&word_over);
+
     add_word(&word_plus);
     add_word(&word_dot);
     add_word(&word_exit);
+
     add_word(&word_double);
     add_word(&word_quadruple);
 
@@ -182,19 +448,10 @@ int main(void)
         interpret(line);
     }
 
-    // push(3);
-    // run(find("quadruple"));
-    // run(find("double"));
-    // do_dot();
-
-    // char *input = " this is Jeopardy! ";
-    // char **in = &input;
-    // printf("[%s]\n",get_token(in));
-    // printf("[%s]\n",get_token(in));
-    // printf("[%s]\n",get_token(in));
-    // printf("[%s]\n",get_token(in));
-    // printf("[%s]\n",get_token(in));
-
-    // interpret("100 QUADRUPLE .");
     return 0;
 }
+
+// TODO add flags:
+	// .set F_IMMED,0x80
+	// .set F_HIDDEN,0x20
+	// .set F_LENMASK,0x1f	// length mask
